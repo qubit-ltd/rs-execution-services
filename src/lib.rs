a5e9b662a7daa4ad55757494385be779d28fb4d2
Haixing Hu <starfish.hu@gmail.com>
2026-05-02 12:30:09 +0800

    chore: standardize attribution and bump version

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 /*******************************************************************************
  *
- *    Copyright (c) 2025 - 2026.
- *    Haixing Hu, Qubit Co. Ltd.
+ *    Copyright (c) 2025 - 2026 Haixing Hu.
  *
- *    All rights reserved.
+ *    SPDX-License-Identifier: Apache-2.0
+ *
+ *    Licensed under the Apache License, Version 2.0.
  *
  ******************************************************************************/
 //! # Qubit Execution Services
@@ -11,9 +12,6 @@
 //! Aggregated execution services facade for blocking, CPU-bound, Tokio blocking,
 //! and async IO tasks.
 //!
-//! # Author
-//!
-//! Haixing Hu
 
 mod execution_services;
 
